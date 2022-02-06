453271aa3da096e943a71a608e27be5ab02e98cd
ThuHtetDev <thuhtettun.dev@gmail.com>
2022-02-06 19:26:15 +0630

    modify_head_name_in_index.blade

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <body>
 <div class="container">
-	<h1>Landing Page</h1>
+	<h1>Todo Application</h1>
 	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod anim id est laborum.</p>
 	
 </div>
